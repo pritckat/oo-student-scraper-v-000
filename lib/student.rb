ed5197b3967477d5ac4a191eddb9b18929037926
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 17:01:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,12 @@ class Student
 
   attr_accessor :name, :location, :profile_url
 
+  @@all
+
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
     @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 end
