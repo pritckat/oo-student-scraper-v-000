3c2f5b68acb8a45997fb1d942ca22481b4b2eb86
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 16:56:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,7 +38,7 @@ class Scraper
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
-    student  
+    student
   end
 
 end
