931683c2cc7626749330a37e9b764c332641c7ec
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 16:50:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,13 +10,16 @@ class Scraper
 
     doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
-        student_profile_link = "#{student.attr('href')}"
+        student_profile_url = "#{student.attr('href')}"
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
       end
     end
     students
+  end
+
+  def scrape_profile_page(profile_url)
   end
 
 end
