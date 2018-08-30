7e21da678ac50a5c3c8683c247bd8290ff62aaa8
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 16:44:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,19 @@
 require 'open-uri'
+require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page
-    
+  def self.scrape_index_page(index_url)
+
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+
+    binding.pry
+
   end
-  
+
 end
+
+#name:
+#location
+#profile_url
